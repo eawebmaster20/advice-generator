4ac926abef366bc5d688b21b7e5c55cd0f2915de
eawebmaster20 <eaweb20@gmail.com>
2024-06-19 09:59:11 -0700

    feat: added an async function call to trigger api call and assigned the response text to the dom object

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ document.getElementById('btnNewAdvice').addEventListener('click', ()=>{
 })
 
 
+/************************************************************************************
+ * ASYNC AWAIT
+ ************************************************************************************/
+async function getInitialAdvice() {
+    try {
+      document.getElementById('adviceTxt').innerText = "Loading...";
+      document.getElementById('btnNewAdvice').style.display = 'none'
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      document.getElementById('advice-number').innerText = response.data.slip.id;
+      document.getElementById('adviceTxt').innerText = `"${response.data.slip.advice}"`;
+    } catch (error) {
+      alert(error.message)
+      console.error(error);
+    }
+    finally{
+        document.getElementById('btnNewAdvice').style.display = 'flex'
+        console.log('Api call completed!');
+    }
+  }
+getInitialAdvice();
